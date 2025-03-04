81c348b90779c16c37cd775a585d87a57b4f5e92
gfreddi <gonzalo.freddi@itecnis.com>
2025-03-04 17:51:09 -0300

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,34 @@
-import 'aos/dist/aos.css'; 
-import AOS from 'aos';
-AOS.init()
+import "aos/dist/aos.css";
+import AOS from "aos";
+AOS.init();
 export default function About() {
-    return (
-      <>
-      <div className='text-white w-screen flex flex-col justify-center items-center  mt-6 '>
-            <h2 id="About" className=" text-2xl  md:text-6xl text-center mt-6 mb-6 ">
-                    Acerca de mí
-             </h2>
-        <section className=' w-2/3 flex flex-col md:block '>
-
-
-        <img  src="https://avatars.githubusercontent.com/u/119691309?s=400&u=5ec38e2765ba260900dd8093ad9d3cd644ca350a&v=4" alt="Mi foto de perfil" className=' md:float-left  md:h-56 md:mr-2 mb-6 md:mb-0' />
-
-   
-        <p className="lg:text-4xl md:text-2xl text-gray-300 font-medium">
-                  Soy Gonzalo, un apasionado desarrollador web full stack con una sólida experiencia en la creación de proyectos tanto en solitario como en equipos colaborativos. Mi enfoque se centra en el desarrollo de aplicaciones web atractivas y funcionales. Estoy capacitado para la creación de Single Page Applications (SPA), lo que significa que puedo desarrollar aplicaciones web interactivas y fluidas que ofrecen una experiencia de usuario excepcional. Mi conjunto de habilidades abarca desde la creación de interfaces de usuario atractivas con HTML, CSS y JavaScript, hasta el desarrollo de servidores y bases de datos robustos para respaldar estas aplicaciones
-         </p>
-
-        </section>
-      </div>
-      </>
-    );
-  }
-  
+	return (
+		<>
+			<div className="text-white w-screen flex flex-col justify-center items-center  mt-6 ">
+				<h2
+					id="About"
+					className=" text-2xl  md:text-6xl text-center mt-6 mb-6 "
+				>
+					Acerca de mí
+				</h2>
+				<section className="w-2/3 min-auto flex flex-col md:flex-row items-center justify-center text-center">
+					<img
+						src="https://avatars.githubusercontent.com/u/119691309?s=400&u=5ec38e2765ba260900dd8093ad9d3cd644ca350a&v=4"
+						alt="Mi foto de perfil"
+						className="md:h-56 md:mr-2 mb-6 md:mb-0"
+					/>
+					<div className="flex flex-col justify-center flex-1">
+						<p className="lg:text-2xl md:text-xl text-gray-300 font-sans font-extralight p-4">
+							Desarrollador Full Stack con experiencia en Python, React, Odoo,
+							Django, Ionic-Angular, Node y PostgreSQL. Apasionado por el
+							desarrollo de software, la optimización de sistemas y la mentoría
+							de nuevos compañeros. Cuento con habilidades en el diseño y
+							desarrollo de soluciones escalables, integración de sistemas y
+							mantenimiento de aplicaciones.
+						</p>
+					</div>
+				</section>
+			</div>
+		</>
+	);
+}
